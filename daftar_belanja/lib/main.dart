4e20c1107370adc9a7bb90e03f0ad1f9df87c755
Ayuuoktavianiiiii <126056969+Ayuuoktavianiiiii@users.noreply.github.com>
2025-04-09 20:21:44 +0700

    latihan

---
+++ daftar_belanja/lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:fasum_app/screens/shopping_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Daftar Belanja',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const ShoppingListScreen(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
---
+++ daftar_belanjaa/android/app/src/main/kotlin/com/example/daftar_belanjaa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.daftar_belanjaa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
